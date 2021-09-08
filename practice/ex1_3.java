d86699c2472854e741fe8ca1f1a9c302ef8a6da3
Ryustam <ryustam@naver.com>
2021-09-08 14:21:20 +0900

    Update ex1_3.java

--- practice/ex1_3.java
+++ practice/ex1_3.java
@@ -3,25 +3,28 @@ package practice;
 import java.util.Scanner;
 
 public class ex1_3 {
-
+	
 	public static void main(String[] args) {
 
-	heightAndAge();	
+	Scanner sc = new Scanner(System.in);
 		
-	}
-	public static void heightAndAge() {
+	double height;
+	int age;
+		
+	System.out.println("나이를 입력하세요");
+	age = sc.nextInt();
+	System.out.println("키를 입력하세요");
+	height = sc.nextDouble();	
+	byEveryone(age,height);
 	
-		double height;
-		int age;
+	sc.close();
 		
-		Scanner sc = new Scanner(System.in);
-		System.out.println("나이를 입력하세요");
-		age = sc.nextInt();
-		System.out.println("키를 입력하세요");
-		height = sc.nextDouble();
-		
-		System.out.printf("나이: %d  키: %.1f",age,height);
-		
-		sc.close();
 	}
+	public static void byEveryone(int age, double height) {
+		System.out.printf("나이: %d  키: %.1f",age,height);
+	}
+	public static void byEveryone() {
+		System.out.println("다음에 뵙겠습니다.");
+	}
+	
 }
